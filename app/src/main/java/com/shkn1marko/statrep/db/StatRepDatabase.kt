56b95837a2afd6d2658ce40b6d5025c79ed111ef
shkn1-marko <shkn1.marko@gmail.com>
2026-09-22 12:51:42 +0200

    feat: add singleton instance builder to StatRepDatabase

--- app/src/main/java/com/shkn1marko/statrep/db/StatRepDatabase.kt
+++ app/src/main/java/com/shkn1marko/statrep/db/StatRepDatabase.kt
@@ -1,6 +1,8 @@
 package com.shkn1marko.statrep.db
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
@@ -8,4 +10,18 @@ import androidx.room.TypeConverters
 @TypeConverters(StageStatusConverters::class)
 abstract class StatRepDatabase : RoomDatabase() {
     abstract fun deployStatusDao(): DeployStatusDao
+
+    companion object {
+        @Volatile
+        private var instance: StatRepDatabase? = null
+
+        fun getInstance(context: Context): StatRepDatabase =
+            instance ?: synchronized(this) {
+                instance ?: Room.databaseBuilder(
+                    context.applicationContext,
+                    StatRepDatabase::class.java,
+                    "statrep.db"
+                ).build().also { instance = it }
+            }
+    }
 }
