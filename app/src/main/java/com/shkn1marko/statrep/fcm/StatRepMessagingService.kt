6e5d20911f54827aea9d3ffc8ae85a28f3e8f59f
shkn1-marko <shkn1.marko@gmail.com>
2026-09-24 19:33:08 +0200

    feat: insert received deploy status into local database

--- app/src/main/java/com/shkn1marko/statrep/fcm/StatRepMessagingService.kt
+++ app/src/main/java/com/shkn1marko/statrep/fcm/StatRepMessagingService.kt
@@ -3,10 +3,19 @@ package com.shkn1marko.statrep.fcm
 import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
+import com.shkn1marko.statrep.db.DeployStatusRepository
+import com.shkn1marko.statrep.db.StatRepDatabase
 import com.shkn1marko.statrep.model.DeployStatus
 
 class StatRepMessagingService : FirebaseMessagingService() {
+
+    private val repository by lazy {
+        DeployStatusRepository(StatRepDatabase.getInstance(this).deployStatusDao())
+    }
 
     override fun onRegistered(installationId: String) {
         super.onRegistered(installationId)
@@ -19,6 +28,9 @@ class StatRepMessagingService : FirebaseMessagingService() {
         val status = DeployStatus.fromData(message.data)
         if (status != null) {
             Log.d(TAG, "Parsed DeployStatus: $status")
+            CoroutineScope(Dispatchers.IO).launch {
+                repository.insert(status)
+            }
         } else {
             Log.w(TAG, "Failed to parse DeployStatus from data: ${message.data}")
         }
