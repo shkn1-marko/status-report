9619c6fc4fc570e7c01ffddc41e71eadc0ecb9d1
shkn1-marko <shkn1.marko@gmail.com>
2026-09-24 10:20:32 +0200

    feat: add unstyled deploy status list UI

--- app/src/main/java/com/shkn1marko/statrep/MainActivity.kt
+++ app/src/main/java/com/shkn1marko/statrep/MainActivity.kt
@@ -4,44 +4,53 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+
 import com.shkn1marko.statrep.ui.theme.StatRepTheme
+import com.shkn1marko.statrep.ui.StatRepViewModel
+import com.shkn1marko.statrep.model.DeployStatus
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: StatRepViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             StatRepTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    val deployStatuses by viewModel.deployStatuses.collectAsState()
+                    DeployStatusList(
+                        statuses = deployStatuses,
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
     }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.deleteExpired()
+    }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    StatRepTheme {
-        Greeting("Android")
+fun DeployStatusList(statuses: List<DeployStatus>, modifier: Modifier = Modifier) {
+    LazyColumn(modifier = modifier) {
+        items(statuses) { status ->
+            Text("${status.name} - ${status.buildStatus} / ${status.deployStatus}")
+        }
     }
 }
